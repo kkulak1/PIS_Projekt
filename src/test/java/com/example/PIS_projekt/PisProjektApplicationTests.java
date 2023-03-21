fdb227432e06f66ebc47c3aa9b17704a39acf40f
kkulak1 <01169200@pw.edu.pl>
2023-03-21 21:13:51 +0100

    Project Created

---
+++ src/test/java/com/example/PIS_projekt/PisProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PIS_projekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PisProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
