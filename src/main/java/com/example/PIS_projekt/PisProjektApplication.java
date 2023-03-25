2c07201f0297e5ce67becd3646c7203541287d11
kkulak1 <01169200@pw.edu.pl>
2023-03-25 12:54:07 +0100

    PIS-17 first test

--- src/main/java/com/example/PIS_projekt/PisProjektApplication.java
+++ src/main/java/com/example/PIS_projekt/PisProjektApplication.java
@@ -2,12 +2,18 @@ package com.example.PIS_projekt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class PisProjektApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PisProjektApplication.class, args);
 	}
 
+	@GetMapping("/")
+	public String firstTest() {
+		return "First test";
+	}
 }
