717176d6d2c20c0df028c6de3b5c3f218dfed6b5
kkulak1 <01169200@pw.edu.pl>
2023-05-17 16:21:40 +0200

    added basic cost implementation

--- src/main/java/RecipeBook/cost/Cost.java
+++ src/main/java/RecipeBook/cost/Cost.java
@@ -1,2 +1,28 @@
-package RecipeBook.cost;public class Cost {
+package RecipeBook.cost;
+
+import RecipeBook.ingredient.Ingredient;
+import jakarta.persistence.*;
+
+public class Cost {
+    @SequenceGenerator(
+            name = "cost_sequence",
+            sequenceName = "cost_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "cost_sequence"
+    )
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(
+            nullable = false,
+            name = "ingredient_id"
+    )
+    private Ingredient ingredient;
+
+    private Long costvalue;
+    private String costUnit;
 }
