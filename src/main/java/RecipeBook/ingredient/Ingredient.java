ffefe229140b26461d21fe28af46d11499db5be9
kkulak1 <01169200@pw.edu.pl>
2023-05-17 16:22:13 +0200

    added basic ingredient implementation

--- src/main/java/RecipeBook/ingredient/Ingredient.java
+++ src/main/java/RecipeBook/ingredient/Ingredient.java
@@ -1,2 +1,38 @@
-package RecipeBook.ingredient;public class Ingredient {
+package RecipeBook.ingredient;
+
+import RecipeBook.recipe.Recipe;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Ingredient {
+    @SequenceGenerator(
+            name = "ingredient_sequence",
+            sequenceName = "ingredient_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "ingredient_sequence"
+    )
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "recipe_id"
+    )
+    private Recipe recipe;
+
+    private String type;
+    private Long amount;
+    private String unit;
+    private String name;
+    private String description;
 }
