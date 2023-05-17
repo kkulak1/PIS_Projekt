f2834d010a264c34ae2d3912922513d8be35f162
kkulak1 <01169200@pw.edu.pl>
2023-05-17 16:21:24 +0200

    added basic step implementation

--- src/main/java/RecipeBook/step/Step.java
+++ src/main/java/RecipeBook/step/Step.java
@@ -1,2 +1,33 @@
-package RecipeBook.step;public class Step {
+package RecipeBook.step;
+
+import RecipeBook.recipe.Recipe;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Step {
+    @SequenceGenerator(
+            name = "step_sequence",
+            sequenceName = "step_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "step_sequence"
+    )
+    private Long id;
+    private String steps;
+
+    @OneToOne
+    @JoinColumn(
+            nullable = false,
+            name = "recipe_id"
+    )
+    private Recipe recipe;
 }
