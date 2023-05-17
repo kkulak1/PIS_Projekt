5398be763373848f140fb4057fa54510673f89b0
kkulak1 <01169200@pw.edu.pl>
2023-05-17 16:21:53 +0200

    added basic duration implementation

--- src/main/java/RecipeBook/duration/Duration.java
+++ src/main/java/RecipeBook/duration/Duration.java
@@ -1,2 +1,35 @@
-package RecipeBook.duration;public class Duration {
+package RecipeBook.duration;
+
+import RecipeBook.recipe.Recipe;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Duration {
+    @SequenceGenerator(
+            name = "duration_sequence",
+            sequenceName = "duration_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "duration_sequence"
+    )
+    private Long id;
+
+    Long amountOfTime;
+    String Unit;
+
+    @OneToOne
+    @JoinColumn(
+            nullable = false,
+            name = "recipe_id"
+    )
+    private Recipe recipe;
 }
