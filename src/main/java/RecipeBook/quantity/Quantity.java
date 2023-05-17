027f411eaa9fb04583f5e704ce50709801ea5641
kkulak1 <01169200@pw.edu.pl>
2023-05-17 16:22:28 +0200

    added basic quanity implementation

--- src/main/java/RecipeBook/quantity/Quantity.java
+++ src/main/java/RecipeBook/quantity/Quantity.java
@@ -1,2 +1,35 @@
-package RecipeBook.quantity;public class Quantity {
+package RecipeBook.quantity;
+
+import RecipeBook.ingredient.Ingredient;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Quantity {
+    @SequenceGenerator(
+            name = "quantity_sequence",
+            sequenceName = "quantity_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "quantity_sequence"
+    )
+    private Long id;
+
+    private Long quantityValue;
+    private String quantityUnit;
+
+    @OneToOne
+    @JoinColumn(
+            nullable = false,
+            name = "ingredient_id"
+    )
+    private Ingredient ingredient;
 }
