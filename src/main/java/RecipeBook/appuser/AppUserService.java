e395eb94decf05a579576417a071e12472abed71
kkulak1 <01169200@pw.edu.pl>
2023-05-28 19:59:18 +0200

    added method to extract logged in user

--- src/main/java/RecipeBook/appuser/AppUserService.java
+++ src/main/java/RecipeBook/appuser/AppUserService.java
@@ -1,10 +1,31 @@
 package RecipeBook.appuser;
 
+import RecipeBook.config.JwtService;
+import io.jsonwebtoken.Jwts;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Service
 @RequiredArgsConstructor
 public class AppUserService {
     private final AppUserRepository appUserRepository;
+    private final JwtService jwtService;
+
+    public AppUser findAppUserByUsername(String email) {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+
+    public AppUser getCurrentUser() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization").split(" ")[1];
+
+        AppUser appUser = findAppUserByUsername(jwtService.extractUsername(token));
+
+        return appUser;
+    }
 }
